dba2c6144405bbc4029087f8096464bd6ee030ff
ArtARTs36 <temicska99@mail.ru>
2021-05-25 02:07:14 +0300

    add gitattributes

--- src/Interfaces/PushValidatorInterface.php
+++ src/Interfaces/PushValidatorInterface.php
@@ -4,10 +4,6 @@ namespace ArtARTs36\PushAllSender\Interfaces;
 
 use ArtARTs36\PushAllSender\Push;
 
-/**
- * Interface PushValidatorInterface
- * @package ArtARTs36\PushAllSender\Interfaces
- */
 interface PushValidatorInterface
 {
     public function validate(Push $push): bool;
